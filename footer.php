65fdc9375f36e0e24ed401cd3229cf11cbe3c69b
nicolasblancom <nicolasblancom@gmail.com>
2016-10-10 07:50:19 +0200

    add 'ninja forms' plugin contact form on modal

--- footer.php
+++ footer.php
@@ -5,6 +5,26 @@
         </footer>
     </div> <!-- /container -->
 
+    <!-- contact form modal window -->
+    <div class="modal fade" id="contactForm" tabindex="-1" role="dialog" aria-labelledby="contactForm">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="contacFormTitle">Contact Me</h4>
+                </div>
+                <div class="modal-body">
+                    <?php
+                    if( function_exists( 'ninja_forms_display_form' ) ){ ninja_forms_display_form( 1 ); }
+                    ?>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <?php
 
     // wp_footer action hook
